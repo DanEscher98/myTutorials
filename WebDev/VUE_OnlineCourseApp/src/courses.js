46c4ced61f040da99d7cb558ddc038ff03be1cdc
Danyiel Colin <danyiel5978@gmail.com>
2022-07-10 00:50:03 -0500

    Working site

---
+++ WebDev/VUE_OnlineCourseApp/src/courses.js
@@ -0,0 +1,42 @@
+const courses = [
+  {
+    id : 1,
+    title : "Philosophy for Beginners",
+    description : "Phasellus ac tellus tincidunt...",
+    lessons : [
+      {
+        id : 1,
+        title : "Welcome to the course",
+        description : "Lorem ipsum dolor sit amet",
+        vimeoId : 76979871
+      },
+      {
+        id : 2,
+        title : "What it means to mean",
+        description : "Lorem ipsum dolor sit amet",
+        vimeoId : 76979871
+      }
+    ]
+  },
+  {
+    id : 2,
+    title : "Advanced Mathematics for Undegrads",
+    description : "Learn how to build your own demostrations",
+    lessons : [
+      {
+        id : 1,
+        title : "Welcome to the course",
+        description : "Presentation of the material",
+        vimeoId : 76979871
+      },
+      {
+        id : 2,
+        title : "Why logic matters",
+        description : "Brief explanation of the history of the logic",
+        vimeoId : 76979871
+      }
+    ]
+  }
+];
+
+export default courses
