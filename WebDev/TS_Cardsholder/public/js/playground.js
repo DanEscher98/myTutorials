4fe564c478fd60b83a53b6e2b74d128df238b422
Danyiel Colin <danyiel5978@gmail.com>
2022-07-12 02:50:58 -0500

    gitignore files removed

--- WebDev/TS_Cardsholder/public/js/playground.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function myTest(args) {
-    if (args.name) {
-        return "Hello ".concat(args.name);
-    }
-    return "Hello Word";
-}
-console.log(myTest({ id: 1, name: "Daniel" }));
