a894fcd4b934dccc82eeefa32fcd08b736ddbcab
danyiel5978@gmail.com <danyiel5978@gmail.com>
2022-11-20 22:03:24 -0600

    conway: added func Universe::{new,tick,count_live_neighbours}

--- BackEnd/rsbook-wasmconway/src/lib.rs
+++ BackEnd/rsbook-wasmconway/src/lib.rs
@@ -1,3 +1,10 @@
+//! # Conway Game of Life rules
+//! Any live cell with:
+//! 1. fewer than 2 live neighbours dies
+//! 2. 2 or 3 live neighbourse lives on to the next gen
+//! 3. more than 3 live neighbours dies
+//! Any dead cell with:
+//! 4. exactly three live neighbours becomes a live cell
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -9,11 +16,64 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}! Lets do some life.", name));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>
+}
+
+#[wasm_bindgen]
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn count_live_neighbor(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (col + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next_cells: Vec<Cell> =
+            Vec::with_capacity((self.width * self.height) as usize);
+        
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let current_cell = self.cells[idx];
+                let live_neighbours = self.count_live_neighbor(row, col);
+
+                let next_cell_state = match (current_cell, live_neighbours) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise
+                };
+                next_cells[idx] = next_cell_state;
+            }
+        }
+        self.cells = next_cells;
+    }
+
 }
