6a9d8ab460e901de8ed0304e774711b211bbff30
Danyiel Colin <danyiel5978@gmail.com>
2022-12-21 23:39:21 -0600

    Persistent singly linked stack

---
+++ Algorithms/book_manylists_rs/src/c_persistent_singly_linked_stack.rs
@@ -0,0 +1,59 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List { head: None }
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn prepend(&self, elem: T) -> Self {
+        List { head: Some(Rc::new(Node {
+            elem,
+            next: self.head.clone()
+        }))}
+    }
+    pub fn tail(&self) -> Self {
+        List {
+            head: self.head.as_ref().and_then(|node|
+                node.next.clone()
+            )
+        }
+    }
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
