1a91ef17ddd37681c9c02fdfc60be1b3e4053d9a
Danyiel Colin <danyiel5978@gmail.com>
2022-12-24 19:39:21 -0600

    Bad safe deque

---
+++ Algorithms/book_manylists_rs/src/d_bad_safe_deque.rs
@@ -0,0 +1,203 @@
+use std::rc::Rc;
+use std::cell::{Ref, RefMut, RefCell};
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None
+        }))
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List { head: None, tail: None }
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+    pub fn pop_front(&mut self) -> Option<T> {
+        // neet to take the old head, enuring it's -2
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    // not emptying list
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                    // total: -2 old, +0 new
+                }
+                None => {
+                    // emptying list
+                    self.tail.take();
+                    // total: -2 old, (no new)
+                }
+            }
+            // intention: old_head.elem
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+    pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
+}
+
+
+macro_rules! list {
+    ($($x:expr),*) => {
+        {
+            let mut ls = List::new();
+            $(
+                ls.push_front($x);
+            )*
+            ls
+        }
+    };
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list: List<i32> = List::new();
+
+        assert_eq!(list.pop_front(), None);
+
+        let mut list = list![1, 2, 3];
+
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        list.push_front(4);
+
+        assert_eq!(list.pop_front(), Some(4));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let list: List<u8> = List::new();
+        assert!(list.peek_front().is_none());
+        let mut list = list![0, 1, 2];
+        assert_eq!(&*list.peek_front().unwrap(), &2);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 2);
+        assert_eq!(&*list.peek_back().unwrap(), &0);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 0);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut iter = list![1, 2, 3].into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+    }
+}
+
--- Algorithms/book_manylists_rs/src/lib.rs
+++ Algorithms/book_manylists_rs/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod a_bad_singly_linked_stack;
 pub mod b_ok_singly_linked_stack;
 pub mod c_persistent_singly_linked_stack;
+pub mod d_bad_safe_deque;
+pub mod e_ok_safe_deque;
 
