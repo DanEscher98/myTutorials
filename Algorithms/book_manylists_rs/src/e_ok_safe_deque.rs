126d30489b20219f7fbdddc8c7765c6c8e839793
Danyiel Colin <danyiel5978@gmail.com>
2022-12-24 20:05:14 -0600

    Ok safe deque: finished

--- Algorithms/book_manylists_rs/src/e_ok_safe_deque.rs
+++ Algorithms/book_manylists_rs/src/e_ok_safe_deque.rs
@@ -55,11 +55,76 @@ impl<T> List<T> {
             }
         }
     }
+    pub fn peek(&self) -> Option<&T> {
+        unsafe {
+            self.head.as_ref().map(|node| &node.elem)
+        }
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe {
+            self.head.as_mut().map(|node| &mut node.elem)
+        }
+    }
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe {
+            Iter { next: self.head.as_ref() }
+        }
+    }
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        unsafe {
+            IterMut { next: self.head.as_mut() }
+        }
+    }
 }
 
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while self.pop().is_some() {}
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.map(|node| {
+                self.next = node.next.as_ref();
+                &node.elem
+            })
+        }
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_mut();
+                &mut node.elem
+            })
+        }
     }
 }
 
@@ -79,5 +144,21 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn miri_food() {
+        let mut list = List::new();
+
+        list.push(0);
+        list.push(1);
+        assert_eq!(list.pop(), Some(0));
+        list.push(2);
+        assert_eq!(list.peek(), Some(&1));
+        if let Some(num) = list.peek_mut() {
+            *num *= 10;
+        }; 
+        assert_eq!(list.peek(), Some(&10))
+
     }
 }
