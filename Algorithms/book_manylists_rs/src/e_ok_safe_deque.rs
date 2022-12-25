1c842b561130b54ae28d06e137621f51978befc5
Danyiel Colin <danyiel5978@gmail.com>
2022-12-24 19:39:36 -0600

    Ok safe deque

---
+++ Algorithms/book_manylists_rs/src/e_ok_safe_deque.rs
@@ -0,0 +1,83 @@
+use std::ptr;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>
+    //tail: Option<&'a mut Node<T>>
+}
+
+type Link<T> = *mut Node<T>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List { head: ptr::null_mut(), tail: ptr::null_mut() }
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn push(&mut self, elem: T) {
+        unsafe {
+            // immediately convert the `Box` into a raw pointer
+            let new_tail = Box::into_raw(Box::new(Node {
+                elem,
+                next: ptr::null_mut()
+            }));
+
+            if !self.tail.is_null() {
+                (*self.tail).next = new_tail;
+            } else {
+                self.head = new_tail;
+            }
+            self.tail = new_tail;
+        }
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        unsafe {
+            if self.head.is_null() {
+                None
+            } else {
+                // RISE FROM THE GRAVE
+                let head = Box::from_raw(self.head);
+                self.head = head.next;
+                
+                if self.head.is_null() {
+                    self.tail = ptr::null_mut();
+                }
+                Some(head.elem)
+            }
+        }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(0);
+        list.push(1);
+        assert_eq!(list.pop(), Some(0));
+        list.push(2);
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), None);
+    }
+}
